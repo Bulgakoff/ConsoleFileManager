d69e6ce74422e889adbb1131cad7894cfdf8354f
Bulgakoff <hlbu@yandex.ru>
2019-12-01 21:22:25 +0300

    add commit

--- lesson01/step1_ex.py
+++ lesson01/step1_ex.py
@@ -1,6 +1,3 @@
-
-
-
 # функция для создания файла
 import os
 
@@ -22,17 +19,24 @@ def info_path(files_only=False, fo_only=False):
     result = os.listdir()
     result1 = os.listdir()
 
-    result = [f for f in result if os.path.isfile(f)]
-    result1 = [fo for fo in result1 if os.path.isdir(fo)]
+    result = [f for f in result if os.path.isdir(f)]
+
     print(result)
-    print(result1)
+
+
+def del_ff(name):
+    if os.path.isdir(name):
+        os.rmdir(name)  # УДАЛЯЕТ ТОЛЬКО ПАПКИ
+    else:
+        os.remove('text.js')
 
 
 # что бы в другом модуле не запускался код
 if __name__ == '__main__':
     create_file('text.js')
     create_file('text.js', 'some text')
-    create_folder('new_folder2')
+    create_folder('new_folder4')
+    del_ff('new_folder3')
 
     info_path(True)
-
+# os.remove('text.js')
